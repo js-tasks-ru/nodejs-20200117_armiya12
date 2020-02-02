786e85e69b1b1933bf9de5265017527b649e46bd
himyulian <armiya12@mail.ru>
2020-02-02 20:25:16 +0300

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -7,10 +7,8 @@ const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-  console.log("TCL: pathname", pathname)
 
   const filepath = path.join(__dirname, 'files', pathname);
-  console.log("TCL: filepath", filepath)
 
   switch (req.method) {
     case 'GET':

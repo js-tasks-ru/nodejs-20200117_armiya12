3ec920b64175180def9b2ab14b6060cc66ad80d6
himyulian <armiya12@mail.ru>
2020-02-11 20:39:24 +0300

    6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,7 @@
+const Category = require('../models/Category');
+const { mapCategory } = require('../mappers/mappers');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const data = await Category.find();
+  ctx.body = { categories: data.map(mapCategory) };
 };
--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,33 @@
+const Product = require('../models/Product.js');
+const { mapProduct } = require('../mappers/mappers');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  const subcategory = ctx.request.query.subcategory;
+  const params = subcategory ? { subcategory: subcategory } : null;
+
+  if (!params) {
+    return next();
+  }
+  const data = await Product.find(params);
+  ctx.body = {
+    products: data.map(mapProduct),
+  };
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const data = await Product.find();
+  ctx.body = {
+    products: data.map(mapProduct),
+  };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const productId = ctx.request.params;
+  const data = await Product.findById(productId);
+  if (!data) {
+    ctx.throw(404, 'Product Not Found');
+  }
+  ctx.body = {
+    product: mapProduct(data),
+  };
 };
-

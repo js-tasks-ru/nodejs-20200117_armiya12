3fdd473c360b9b09f7e655bc085d22572bd1e1df
himyulian <armiya12@mail.ru>
2020-02-11 20:53:25 +0300

    6-module 2-rask

--- 6-module/2-task/app.js
+++ 6-module/2-task/app.js
@@ -15,7 +15,7 @@ app.use(async (ctx, next) => {
       ctx.status = err.status;
       ctx.body = {error: err.message};
     } else {
-      console.error(err);
+      // console.error(err);
       ctx.status = 500;
       ctx.body = {error: 'Internal server error'};
     }

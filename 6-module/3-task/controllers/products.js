31a44fdb97e2dea5a502270077a2b1c2639212d9
himyulian <armiya12@mail.ru>
2020-02-12 20:07:34 +0300

    6-module 3-task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,10 +1,10 @@
 
 const Product = require('../models/Product');
-const mapProduct = require('../mappers/mappers');
+const { mapProduct } = require('../mappers/mappers');
 
 module.exports.productsByQuery = async (ctx, next) => {
   const query = ctx.request.query.query;
-  const data = await Product.find({$text: {$search: query}}).sort({ title: 1, description: -1 });
-  const result = data ? data.map(mapProduct) : [];
+  const data = await Product.find({$text: {$search: query}});
+  const result = data.map(mapProduct);
   ctx.body = {products: result};
 };

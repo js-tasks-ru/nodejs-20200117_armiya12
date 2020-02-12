2369b8d46bbd2fd7fa7e0df4ac3bc1ebd4c3478a
himyulian <armiya12@mail.ru>
2020-02-12 08:37:55 +0300

    6-module 3-task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,3 +1,10 @@
-module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/mappers');
+
+module.exports.productsByQuery = async (ctx, next) => {
+  const query = ctx.request.query.query;
+  const data = await Product.find({$text: {$search: query}});
+  const result = data.map(mapProduct);
+  ctx.body = {products: result};
 };

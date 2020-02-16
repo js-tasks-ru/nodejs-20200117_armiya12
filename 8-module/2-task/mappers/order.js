2a7c8af8f54a867b8367dd4d3173bfe118aa3cf9
himyulian <armiya12@mail.ru>
2020-02-16 20:36:08 +0300

    8-module 2-task

---
+++ 8-module/2-task/mappers/order.js
@@ -0,0 +1,17 @@
+module.exports = function mapOrder(order) {
+    return {
+        id: order.id,
+        user: order.user,
+        product: {
+            id: order.product.id,
+            title: order.product.title,
+            images: order.product.images,
+            category: order.product.category,
+            subcategory: order.product.subcategory,
+            price: order.product.price,
+            description: order.product.description,
+        },
+        phone: order.phone,
+        address: order.address,
+    };
+};

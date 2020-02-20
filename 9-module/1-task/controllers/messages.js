f3725102c73975b0c8b6c8557a6c90196fedb1ab
himyulian <armiya12@mail.ru>
2020-02-20 12:13:22 +0300

    9-module 1-task

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -1,5 +1,7 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  if (!ctx.user) ctx.throw(401, 'Not authorized');
+  const messages = await Message.find({ user: ctx.user.displayName }).select('date text id user').limit(20).exec();
+  ctx.body = { messages };
 };

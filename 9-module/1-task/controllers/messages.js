d8c8ca88fd63cb17acd58b90c5a717def41a8c77
himyulian <armiya12@mail.ru>
2020-02-20 20:06:56 +0300

    9-module 1-task

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -1,7 +1,8 @@
 const Message = require('../models/Message');
+const mapMessage = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  if (!ctx.user) ctx.throw(401, 'Not authorized');
+  if (!ctx.user) ctx.throw(401, 'Пользователь не залогинен');
   const messages = await Message.find({ user: ctx.user.displayName }).select('date text id user').limit(20).exec();
-  ctx.body = { messages };
+  ctx.body = { messages: messages.map(mapMessage) };
 };
